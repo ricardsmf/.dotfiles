8e6636be2867012bb63571f1a96c15e7a598d8a4
ricardosmf <ricardosmf@outlook.com>
2026-06-23 14:08:04 +0100

    add pnpm completions

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -11,3 +11,7 @@ set -gx MANPAGER 'nvim +Man!'
 fish_add_path ~/.dotfiles
 
 alias lg='lazygit'
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :

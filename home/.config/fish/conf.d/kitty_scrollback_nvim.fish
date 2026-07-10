74f94acaf3df949c4035ea55fd2b38e61b333c3c
ricardosmf <ricardosmf@outlook.com>
2026-07-10 12:50:41 +0100

    add kitty scrollback

---
+++ home/.config/fish/conf.d/kitty_scrollback_nvim.fish
@@ -0,0 +1,14 @@
+if not status is-interactive
+    return
+end
+function kitty_scrollback_edit_command_buffer
+  set --local --export VISUAL '/Users/ricardoferreira/.local/share/nvim/lazy/kitty-scrollback.nvim/scripts/edit_command_line.sh'
+  edit_command_buffer
+  commandline ''
+end
+bind --mode default \ee kitty_scrollback_edit_command_buffer
+bind --mode visual \ee kitty_scrollback_edit_command_buffer
+bind --mode insert \ee kitty_scrollback_edit_command_buffer
+# [optional] pass arguments to kitty-scrollback.nvim in command-line editing mode
+# by using the environment variable KITTY_SCROLLBACK_NVIM_EDIT_ARGS
+# set --global --export KITTY_SCROLLBACK_NVIM_EDIT_ARGS ''

afab5e0168f2664478d0f290640b3d43ccc02405
ricardosmf <ricardosmf@outlook.com>
2026-09-17 17:52:38 +0100

    config paneru

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -70,15 +70,28 @@
       options = {
         focus_follows_mouse = true;
         mouse_follows_focus = true;
-        preset_column_widths = [0.25 0.33 0.5 0.66 0.75];
+        animation_speed = 15;
+        preset_column_widths = [0.25 0.33 0.5 0.66 0.75 1.00];
       };
       bindings = {
         window_focus_west = "alt - h";
         window_focus_east = "alt - l";
+        window_swap_west = "alt - shift - h";
+        window_swap_east = "alt - shift - l";
         window_resize = "alt - r";
+        window_shrink = "alt - s";
         window_center = "alt - c";
         window_balance = "alt - b";
         quit = "ctrl + alt - q";
+      };
+      swipe = {
+          deceleration = 0.4;
+          sensitivity = 0.4;
+          continuous = false;
+          gesture = {
+              fingers_count = 3;
+              direction = "Natural";
+          };
       };
     };
   };
